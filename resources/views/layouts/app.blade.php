a801a1df81ad35ef7836f7a4c5ff0c76bd720076
aabelyaev <a.belyaev87@bk.ru>
2025-09-22 15:29:26 +0500

    Merge remote

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,8 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        <script type="module" src="https://php-project-57-xp8o.onrender.com/build/assets/app-DuPBeUm9.js"></script>
+        <link rel="stylesheet" href="https://php-project-57-xp8o.onrender.com/build/assets/app-CBuHC2us.css">
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
