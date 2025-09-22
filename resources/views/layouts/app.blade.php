8e567164cf4f6a8cf026ec9435a7c13521c11855
aabelyaev <a.belyaev87@bk.ru>
2025-09-22 15:15:07 +0500

    Merge remote

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <link rel="stylesheet" href="public/css/bootstrap/bootstrap.min.css">
+        <link rel="stylesheet" href="public/css/styles.css">
+
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
