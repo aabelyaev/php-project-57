6f0094bdb989362f75c2d7833bf18fe07acde1d5
aabelyaev <a.belyaev87@bk.ru>
2025-09-23 17:19:41 +0500

    Merge remote

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,19 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\URL;
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
+    public function boot(): void
     {
-        //
-    }
+        // Принудительное использование HTTPS для всех URL
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
 
-    /**
-     * Bootstrap any application services.
-     */
-    public function boot()
-    {
+        // Или всегда использовать HTTPS
         URL::forceScheme('https');
     }
-
 }
