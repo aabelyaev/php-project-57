4bc69177d04c03061044bf02dee5a4740e8f61f3
aabelyaev <a.belyaev87@bk.ru>
2025-09-23 13:59:16 +0500

    Merge remote

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,7 +4,6 @@ namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,17 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        // Более надежная проверка окружения
-        if ($this->app->environment('production')) {
-            URL::forceScheme('https');
-
-            // Дополнительно для генерации URL
-            $this->app['request']->server->set('HTTPS', 'on');
+        if (env('APP_ENV') == 'production') {
+            $url->forceScheme('https');
         }
-
-        // Или для любого окружения кроме local
-        // if (!$this->app->environment('local')) {
-        //     URL::forceScheme('https');
-        // }
     }
 }
