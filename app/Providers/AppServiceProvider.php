0392f9e7f74b65782af1fe4b489598536bea0644
aabelyaev <a.belyaev87@bk.ru>
2025-10-06 19:04:14 +0500

    Merge remote

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,9 +20,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url): void
     {
-        if (env('APP_ENV') == 'production') {
-            $url->forceScheme('https');
-        } else {
+        if (env('APP_ENV') != 'production') {
             $url->forceScheme('https');
         }
     }
