e5389f4580081d55885b6fcdce95c4a350d01052
aabelyaev <a.belyaev87@bk.ru>
2025-10-06 23:39:40 +0500

    Merge remote

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
-        <h1 class="font-semibold text-2xl text-gray-800 dark:text-gray-200 leading-tight">
+        <h1 class="max-w-2xl mb-4 text-4xl font-extrabold leading-none tracking-tight md:text-5xl xl:text-6xl dark:text-white">
             {{ __('Привет от Хекслета!') }}
         </h1>
 
-        <p class="text-sm mt-2 text-gray-600 dark:text-gray-300">
+        <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             {{ __('Это простой менеджер задач на Laravel') }}
         </p>
     </x-slot>
