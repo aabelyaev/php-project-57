895a7ea11ae9d19ec284bbf162cda00df4b33b66
aabelyaev <a.belyaev87@bk.ru>
2025-10-06 20:38:31 +0500

    Merge remote

--- app/Models/Label.php
+++ app/Models/Label.php
@@ -6,6 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property \Illuminate\Support\Carbon $created_at
+ * @property \Illuminate\Support\Carbon $updated_at
+ */
 class Label extends Model
 {
     use HasFactory;
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -5,12 +5,15 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 /**
  * @property User $createdBy
  */
 class Task extends Model
 {
+    use HasFactory;
+
     protected $fillable = ['name', 'description', 'status_id', 'assigned_to_id'];
 
     public function status(): BelongsTo
