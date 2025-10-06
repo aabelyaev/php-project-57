14dd532ea7c15175f4459f59eb48129a246e5fba
aabelyaev <a.belyaev87@bk.ru>
2025-10-06 19:47:13 +0500

    Merge remote

--- database/factories/TaskStatusFactory.php
+++ database/factories/TaskStatusFactory.php
@@ -3,24 +3,21 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
-use App\Models\Task;
-use App\Models\User;
-use App\Models\TaskStatus;
 
-class TaskFactory extends Factory
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\TaskStatus>
+ */
+class TaskStatusFactory extends Factory
 {
-    protected $model = Task::class;
-
-    public function definition()
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
     {
         return [
-            'name' => $this->faker->sentence(3),
-            'description' => $this->faker->paragraph(),
-            'status_id' => TaskStatus::factory(),
-            'created_by_id' => User::factory(),
-            'assigned_to_id' => User::factory(),
-            'created_at' => now(),
-            'updated_at' => now(),
+            'name' => fake()->name(),
         ];
     }
 }
